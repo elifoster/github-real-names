50015546e262cff940544c50338f08823de6bea5
elifoster <elifosterwy@gmail.com>
2016-06-18 14:48:43 -0700

    :gem: Add support for issue assignees. Close #6.

--- data/issues.js
+++ data/issues.js
@@ -2,24 +2,40 @@ var authors = document.getElementsByClassName('author');
 
 var doneAuthors = {};
 
-for (var i = 0; i < authors.length; i++) {
-    var author = authors[i];
-    var username = author.textContent;
+var i;
+
+for (i = 0; i < authors.length; i++) {
+    emitGet(authors[i]);
+}
+
+var assignees = document.getElementsByClassName('assignee');
+
+for (i = 0; i < assignees.length; i++) {
+    emitGet(assignees[i]);
+}
+
+function emitGet(person) {
+    var username = person.textContent;
     if (!doneAuthors.hasOwnProperty(username)) {
         self.port.emit('getRealname', username);
     }
 }
 
-function trySetHTML() {
-    for (var i = 0; i < authors.length; i++) {
-        var author = authors[i];
-        // Skip the repo header (e.g., the "elifoster" in "elifoster/github-real-names").
-        if (author.getAttribute('itemprop') == 'author') {
+function setHTMLForAuthors(people) {
+    for (var i = 0; i < people.length; i++) {
+        var person = people[i];
+        if (person.getAttribute('itemprop') == 'author') {
             continue;
         }
-        var username = author.textContent;
-        authors[i].textContent = doneAuthors[username] || username;
+
+        var username = person.textContent;
+        people[i].textContent = doneAuthors[username] || username;
     }
+}
+
+function trySetHTML() {
+    setHTMLForAuthors(authors);
+    setHTMLForAuthors(assignees);
 }
 
 self.port.on('sendRealname', function(obj) {
